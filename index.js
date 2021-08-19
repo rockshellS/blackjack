0db3860aa7875f8af47b58666eee1c8ef60cfc07
rockshellS <spicer.rachelle@gmail.com>
2021-08-19 15:22:25 -0400

    some css

---
+++ index.js
@@ -0,0 +1,25 @@
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+
+
+
+function startGame() {
+    
+    if (sum <= 20 ) {
+        message = "Do you want to draw a new card?"
+    }else if (sum === 21) {
+        message = "WHOOHOO! You've got Balckjack!!"
+        hasBlackJack = true
+    }else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    console.log(message)
+}
+
+
