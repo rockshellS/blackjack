77e7666b18e3d0e8b7e36e2b6b2034b5882d6c1e
rockshellS <spicer.rachelle@gmail.com>
2021-09-13 14:29:23 -0400

    some lodgic

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = 0
+let sum = []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.querySelector("#message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+let player = {
+    name: "Rachelle",
+    chips: 200
+}
+
+let playerEl = document.querySelector("#player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13 ) + 1
+    
+    if (randomNumber > 10 ) {
+        return 10
+    }else if (randomNumber === 1 ) {
+        return 11
+    }else {
+        return randomNumber
+    }
+}
+
 
 function startGame() {
+    isAlive = true
+    
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    
     renderGame()
 }
 
+
 function renderGame(){
-    cardsEl.textContent = "Cards: " + cards[0]+ " " + cards[1]
+    cardsEl.textContent = "Cards: " 
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+
     sumEl.textContent = "Sum:" + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -30,8 +64,13 @@ function renderGame(){
     messageEl.textContent = message
     }
 
+
 function newCard() {
-    let card = 7
-    sum += card
-    renderGame()
+    
+    if (isAlive && hasBlackJack === true) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
